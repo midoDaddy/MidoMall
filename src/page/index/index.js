878cd5ab8bfc4d9092229e5852a92ef35c7a2bd6
midoDaddy <fisheeep@gmail.com>
2017-09-15 17:34:26 +0800

    init webpack

---
+++ src/page/index/index.js
@@ -0,0 +1,6 @@
+/*
+* @Author: midoDaddy
+* @Date:   2017-09-15 17:30:54
+* @Last Modified by:   midoDaddy
+* @Last Modified time: 2017-09-15 17:30:54
+*/
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+/*
+* @Author: midoDaddy
+* @Date:   2017-09-14 14:31:53
+* @Last Modified by:   midoDaddy
+* @Last Modified time: 2017-09-15 17:32:10
+*/
+module.exports = {
+    entry: './src/page/index/index.js',
+    output: {
+        path:  './dist',
+        filename: 'app.js'
+    }
+}
