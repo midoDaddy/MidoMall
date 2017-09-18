fc4268dbad739a8c2de2ccdef9b2c8cfe6458ddd
midoDaddy <fisheeep@gmail.com>
2017-09-18 22:16:23 +0800

    html/css/js loader config

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,5 +2,8 @@
 * @Author: midoDaddy
 * @Date:   2017-09-15 17:30:54
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-15 17:30:54
+* @Last Modified time: 2017-09-18 21:56:29
 */
+require('./index.css')
+
+console.log('i am index')
---
+++ src/page/login/index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: midoDaddy
+* @Date:   2017-09-18 10:18:36
+* @Last Modified by:   midoDaddy
+* @Last Modified time: 2017-09-18 21:56:40
+*/
+console.log('i am login')
