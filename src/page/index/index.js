c1ffe992f447ec093e7cea007140bd0847cbeb46
midoDaddy <fisheeep@gmail.com>
2017-09-20 16:34:57 +0800

    add getUrlParam and renderHtml

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,17 +2,14 @@
 * @Author: midoDaddy
 * @Date:   2017-09-19 09:47:45
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-20 10:35:13
+* @Last Modified time: 2017-09-20 16:29:48
 */
-require('./index.css')
+require('./index.css');
+
 var _util = require('util/main.js');
-_util.request({
-    url: '/product/list.do?keyword=1',
-    success: function(res) {
-        console.log(res)
-    },
-    error: function(errMsg) {
-        console.log(errMsg)
-    }
-});
-console.log('this is index')
+var html = '<div>{{name}}</div>';
+var data = {
+    name: 'fisheep'
+}
+
+console.log(_util.renderHtml(html, data));
--- src/util/main.js
+++ src/util/main.js
@@ -2,10 +2,13 @@
 * @Author: midoDaddy
 * @Date:   2017-09-20 09:39:21
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-20 10:36:32
+* @Last Modified time: 2017-09-20 16:29:34
 */
+var config = {
+    serverHost: ''
+}
+var Hogan = require('hogan');
 var _util = {
-
     //ajax请求
     request: function(param) {
         var _this = this;
@@ -37,6 +40,23 @@ var _util = {
     //统一登录处理
     doLogin: function() {
         window.location.href = './login.html?redirect=' + encodeURIComponent(window.location.href)
+    },
+
+    //获取服务器地址
+    getServerUrl: function(path) {
+        return config.serverHost + path;
+    },
+
+    //获取url参数
+    getUrlParam: function(name) {
+        var pattern = new RegExp('(^|&)' + name + '=([^&]*)(&|$)'),
+            result = window.location.search.substr(1).match(pattern);
+        return result ? result[2] : null;
+    },
+
+    //渲染html模板
+    renderHtml: function(htmlTemplate, data) {
+        return Hogan.compile(htmlTemplate).render(data);
     }
 }
 
