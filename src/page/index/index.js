93b788051d3b7043ed5208243803dc41ec9f15de
midoDaddy <fisheeep@gmail.com>
2017-09-20 17:53:12 +0800

    add tip and validate in util

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,14 +2,10 @@
 * @Author: midoDaddy
 * @Date:   2017-09-19 09:47:45
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-20 16:29:48
+* @Last Modified time: 2017-09-20 16:59:29
 */
 require('./index.css');
 
 var _util = require('util/main.js');
-var html = '<div>{{name}}</div>';
-var data = {
-    name: 'fisheep'
-}
 
-console.log(_util.renderHtml(html, data));
+console.log(_util.validate(13429150313, 'phone'));
--- src/util/main.js
+++ src/util/main.js
@@ -2,7 +2,7 @@
 * @Author: midoDaddy
 * @Date:   2017-09-20 09:39:21
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-20 16:29:34
+* @Last Modified time: 2017-09-20 16:59:00
 */
 var config = {
     serverHost: ''
@@ -42,6 +42,11 @@ var _util = {
         window.location.href = './login.html?redirect=' + encodeURIComponent(window.location.href)
     },
 
+    //返回首页
+    goHome: function() {
+        window.location.href = './index.html'
+    },
+
     //获取服务器地址
     getServerUrl: function(path) {
         return config.serverHost + path;
@@ -57,6 +62,33 @@ var _util = {
     //渲染html模板
     renderHtml: function(htmlTemplate, data) {
         return Hogan.compile(htmlTemplate).render(data);
+    },
+
+    //成功提示
+    successTip: function(msg) {
+        alert(msg || '操作成功')
+    },
+
+    //错误提示
+    errorTip: function(msg) {
+        alert(msg || '操作失败')
+    },
+
+    //通用验证
+    validate: function(value, type) {
+        var value = $.trim(value);
+        //非空验证
+        if ('required' === type) {
+            return !!value;
+        }
+        //手机号验证
+        if ('phone' === type) {
+            return /^1\d{10}$/.test(value)
+        }
+        //邮箱验证
+        if ('mail' === type) {
+            return /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(value)
+        }
     }
 }
 
