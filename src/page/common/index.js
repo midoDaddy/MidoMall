5b9c4b6cc9bc5b414ac4be4cedafddda7522ba60
midoDaddy <fisheeep@gmail.com>
2017-09-24 21:37:49 +0800

    add common footer

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,11 +2,11 @@
 * @Author: midoDaddy
 * @Date:   2017-09-19 10:00:23
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-22 17:11:07
+* @Last Modified time: 2017-09-23 21:20:10
 */
 require('./reset.css');
 require('./layout.css');
 require('node_modules/font-awesome/css/font-awesome.min.css');
-require('page/common/nav-simple/index.js')
-require('page/common/nav/index.js')
-console.log('this is common')
+require('./footer/index.css');
+require('page/common/nav-simple/index.js');
+require('page/common/nav/index.js');
