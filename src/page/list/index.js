e57de5b6344be09e7ed5f0156cc4c82b75dee97b
midoDaddy <fisheeep@gmail.com>
2017-10-09 11:21:02 +0800

    finish list.html

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -2,26 +2,26 @@
 * @Author: midoDaddy
 * @Date:   2017-09-28 22:47:25
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-10-02 23:46:15
+* @Last Modified time: 2017-10-09 10:21:16
 */
 require('./index.css');
 require('page/common/header/index.js');
 require('page/common/nav/index.js');
-
 require('util/unslider/index.js');
 
 var _util       = require('util/main.js'),
     _product    = require('service/product-service.js'),
-    template    = require('./index.string');
+    template    = require('./index.string'),
+    Pagination  = require('util/pagination/index.js');
 
 var page = {
     data: {
         listParam: {
             categoryId: _util.getUrlParam('categoryId') || '',
             keyword:  _util.getUrlParam('keyword') || '',
-            pageNum: 1,
-            pageSize: 20,
-            orderBy: ''
+            pageNum: _util.getUrlParam('pageNum') || 1,
+            pageSize: _util.getUrlParam('pageSize') || 8,
+            orderBy: _util.getUrlParam('orderBy') || 'default',
         }
     },
 
@@ -32,7 +32,8 @@ var page = {
     
     //加载产品列表
     loadList: function() {
-        var pListCon = $('.p-list-con');
+        var pListCon = $('.p-list-con'),
+            _this = this;
         pListCon.html('<div class="loading"></div>'); //加载进度条
         //请求产品列表接口
         _product.getProductList(this.data.listParam, function(res) {
@@ -40,44 +41,71 @@ var page = {
                 list: res.list
             });
             $('.p-list-con').html(html);
+            //加载分页信息
+            _this.loadPagination({
+                pageNum         : res.pageNum,
+                pages           : res.pages,
+                hasPreviousPage : res.hasPreviousPage,
+                prePage         : res.prePage,
+                hasNextPage     : res.hasNextPage,
+                nextPage        : res.nextPage,               
+            });                       
         }, function(errMsg) {
             _util.errorTip(errMsg);
         })
     },
+    
+    //加载分页组件
+    loadPagination: function(cfg) {
+        var _this = this;
+        if (!this.pagination) {
+            this.pagination = new Pagination();
+        }
+        this.pagination.render($.extend({}, cfg, {
+            container   : $('.pagination'),
+            onSelect    : function(pageNum) {
+                    _this.data.listParam.pageNum = pageNum;
+                    _this.loadList();
+                }
+        }))
+    },
+
+    //产品排序
+    sortItem: function(e) {
+        this.data.listParam.pageNum = 1;
+        var $target = $(e.target);       
+        while(!$target.hasClass('sort-item')){
+            $target = $target.parent();
+        }
+        //默认排序
+        if ($target.data('type') === 'default') {
+            if ($target.hasClass('active')) {
+                return;
+            } else {
+                $target.addClass('active')
+                    .siblings('.sort-item').removeClass('active asc desc');
+                this.data.listParam.orderBy = 'default';
+            }               
+        }        
+        //价格排序
+        if ($target.data('type') === 'price') {
+            if (!$target.hasClass('active')) {
+                $target.addClass('active')
+                    .siblings('.sort-item').removeClass('active')
+            }
+            if ($target.hasClass('asc')) {
+                $target.removeClass('asc').addClass('desc');
+                this.data.listParam.orderBy = 'price_desc';
+            } else {
+                $target.removeClass('desc').addClass('asc');
+                this.data.listParam.orderBy = 'price_asc';
+            }                
+        }
+        this.loadList();                
+    },
 
     bindEvent: function() {
-        var _this = this;
-        $('.sort-item').on('click', function(){
-            var $this = $(this);
-            _this.data.listParam.pageNum = 1;
-            
-            //默认排序
-            if ($this.data('type') === 'default') {
-                if ($this.hasClass('active')) {
-                    return;
-                } else {
-                    $this.addClass('active')
-                        .siblings('.sort-item').removeClass('active');
-                    _this.data.listParam.orderBy = 'default';
-                }               
-            }
-            
-            //价格排序
-            if ($this.data('type') === 'price') {
-                if (!$this.hasClass('active')) {
-                    $this.addClass('active')
-                        .siblings('.sort-item').removeClass('active')
-                }
-                if ($this.hasClass('asc')) {
-                    $this.removeClass('asc').addClass('desc');
-                    _this.data.listParam.orderBy = 'price_desc';
-                } else {
-                    $this.removeClass('desc').addClass('asc');
-                    _this.data.listParam.orderBy = 'price_asc';
-                }                
-            }
-            _this.loadList();
-        })
+        $('.sort-item').on('click', this.sortItem.bind(this))           
     },
     
 }
--- src/util/pagination/index.js
+++ src/util/pagination/index.js
@@ -2,65 +2,88 @@
 * @Author: midoDaddy
 * @Date:   2017-10-03 23:32:27
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-10-04 00:10:13
+* @Last Modified time: 2017-10-09 10:20:31
 */
 require('./index.css');
 var _util = require('util/main.js'),
     template = require('./index.string');
 
-var Pagination = function(cfg) {
-    this.data.list = [];
+var Pagination = function() {
     this.cfg = {
         pageNum: 1,
         pageSize: 10,
-        pages: 1,
         pageRange: 10,
-        hasPreviousPage: false,
-        prePage: 0,
-        hasNextPage: false,
-        nextPage: 0
-    }
-    this.CFG = $.extend({}, this.cfg, cfg);
+        container: null,
+        onSelect: null
+    } 
+    this.bindEvent();  
 }
 
 Pagination.prototype = {
 
     constructor: Pagination,
-
+    
+    render: function(cfg) {
+        var _this = this,
+            html = '';
+        this.CFG = $.extend({}, this.cfg, cfg); 
+        if (!(this.CFG.container instanceof jQuery)) {
+            return
+        }
+        if (this.CFG.pages <= 1) {
+            return
+        }       
+        html = _util.renderHtml(template, {
+            list: _this.getListData(),
+            pageNum: _this.CFG.pageNum,
+            pages: _this.CFG.pages
+        });       
+        this.CFG.container.html(html);
+    },
+    
+    //获取分页列表数据
     getListData: function() {
-        var list = this.data.list,
+        var listData = [],
             CFG = this.CFG,
             minPage = Math.max((CFG.pageNum - CFG.pageRange/2), 1),
             maxPage = Math.min((CFG.pageNum + CFG.pageRange/2), CFG.pages);
-        list.push({
+
+        listData.push({
             name: '上一页',
             value: CFG.prePage,
             disabled: !CFG.hasPreviousPage
         });
 
         for (var i = minPage; i <= maxPage; i++) {
-            list.push({
+            listData.push({
                 name: i,
                 value: i,
-                active: i === CFG.pageNum
+                active: (i === CFG.pageNum)
             })
         }
 
-        list.push({
+        listData.push({
             name: '下一页',
             value: CFG.nextPage,
             disabled: !CFG.hasNextPage
         });
-    }
 
-    render: function() {
+        return listData;
+    },
+    
+    //分页点击事件
+    bindEvent: function() {
         var _this = this;
-        var html = _util.renderHtml(template, {
-            list: _this.data.list,
-            pageNum: _this.CFG.pageNum,
-            pages: _this.CFG.pages
-        });
-        $('.pagination').html(html);
+        $(document).on('click', '.pg-item', function(){
+            var $this = $(this);
+            if ($this.hasClass('disabled') || $this.hasClass('active')) {
+                return;
+            }
+            //回调函数，返回页码数据
+            if (typeof _this.CFG.onSelect === 'function') {
+                _this.CFG.onSelect($(this).data('value'));
+            }
+        })
     }
 
 }
